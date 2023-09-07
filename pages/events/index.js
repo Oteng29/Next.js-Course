310646570e5d3f8f91208c975a2de04226a408b6
Oteng29 <otengmilleny29@gmail.com>
2023-09-07 08:45:27 +0200

    Next.js

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Fragment } from 'react';
+
+import { getAllEvents } from '../../dummy-data';
+import EventList from '../../components/events/eventList';
+import EventsSearch from '@/components/events/event-search';
+import { useRouter } from 'next/router';
 
 export default function AllEventPage() {
+  const router = useRouter();
+  const events = getAllEvents();
+
+  function findEventHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
+
   return (
-    <div>
-      <h1>All Events</h1>
-    </div>
+    <Fragment>
+      <EventsSearch onSearch={findEventHandler}/>
+      <EventList items={events}/>
+    </Fragment>
   )
 }
